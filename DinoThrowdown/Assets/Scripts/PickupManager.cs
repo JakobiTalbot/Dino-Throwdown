c2fcc7b5b32b841d9e18b09b3efe0300550ec0b0
T-Scod <36467730+T-Scod@users.noreply.github.com>
2018-10-05 16:24:55 +1000

    Adding weapon size pickup and pickup manager
    Weapon size pickup does not yet affect knock back

---
+++ DinoThrowdown/Assets/Scripts/PickupManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupManager : MonoBehaviour
+{
+    // collection of different pickup types
+    public GameObject[] m_pickupTypes;
+    // time between spawning
+    public float m_fSpawnTime = 10.0f;
+    // collection of spawn locations
+    public Transform[] m_spawnPoints;
+
+    private void Start()
+    {
+        // repeatedly calls the Spawn function
+        InvokeRepeating("Spawn", m_fSpawnTime, m_fSpawnTime);
+    }
+
+    // spawns a pickup at a random location
+    private void Spawn()
+    {
+        // random location index
+        int iSpawnPointIndex = Random.Range(0, m_spawnPoints.Length);
+        // random pickup type index
+        int iPickupTypeIndex = Random.Range(0, m_pickupTypes.Length);
+        // creates the random pickup at the random location
+        Instantiate(m_pickupTypes[iPickupTypeIndex], m_spawnPoints[iSpawnPointIndex].position, m_spawnPoints[iSpawnPointIndex].rotation);
+    }
+}
---
+++ DinoThrowdown/Assets/Scripts/WeaponSize.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSize : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // checks if the colliding object is a player
+        if (other.CompareTag("Player"))
+        {
+            // gets the arm component from the player
+            GameObject arm = other.GetComponent<PlayerMovement>().m_arm;
+            // increases the scale of the object
+            arm.transform.localScale = new Vector3(4.0f, 0.4f, 0.4f);
+            // sets this object to inactive
+            gameObject.SetActive(false);
+        }
+    }
+}
