e7971af74012d7fee0f163334c88b735ed1c51cd
Tory-Kruger <39174598+Tory-Kruger@users.noreply.github.com>
2018-10-07 18:56:13 +1100

    -Added outer ring, large and small pillars for measurements REPLACE WITH NON STUDENT MAYA VERSIONS

--- DinoThrowdown/Assets/Scripts/WeaponSize.cs
+++ DinoThrowdown/Assets/Scripts/WeaponSize.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class WeaponSize : MonoBehaviour
 {
+
     private void OnTriggerEnter(Collider other)
     {
+
+        GameObject.FindGameObjectsWithTag("Player");
+
         // checks if the colliding object is a player
         if (other.CompareTag("Player"))
         {
             // gets the arm component from the player
-            GameObject arm = other.GetComponent<PlayerMovement>().m_arm;
+  //          GameObject arm = other.GetComponent<PlayerController>().m_arm;
             // increases the scale of the object
-            arm.transform.localScale = new Vector3(4.0f, 0.4f, 0.4f);
+  //          arm.transform.localScale = new Vector3(4.0f, 0.4f, 0.4f);
             // sets this object to inactive
-            gameObject.SetActive(false);
+  //          gameObject.SetActive(false);
         }
     }
 }
