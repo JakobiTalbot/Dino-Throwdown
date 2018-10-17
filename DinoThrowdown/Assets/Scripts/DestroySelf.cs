13ad1dc0f00d91ad498fcd28e3ab07e5e68becc1
JakobiTalbot <36465136+JakobiTalbot@users.noreply.github.com>
2018-10-18 09:14:57 +1100

    - Fixed warnings caused by missing script in HitParticle prefab - HitParticle prefab now contains DestroySelf script

---
+++ DinoThrowdown/Assets/Scripts/DestroySelf.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour
+{
+    public float m_nTimeToDestroy = 5.0f;
+	// Use this for initialization
+	void Awake()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        m_nTimeToDestroy -= Time.deltaTime;
+
+        if (m_nTimeToDestroy <= 0.0f)
+        {
+            Destroy(this);
+        }
+	}
+}
