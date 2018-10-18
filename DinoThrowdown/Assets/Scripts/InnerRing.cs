49a28b8f3ba8ef31bbc23cf9d59647d47fa22e92
T-Scod <36467730+T-Scod@users.noreply.github.com>
2018-10-19 10:27:19 +1100

    - Made the drop speed outside the platform adjustable in the inspector

--- DinoThrowdown/Assets/Scripts/InnerRing.cs
+++ DinoThrowdown/Assets/Scripts/InnerRing.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class InnerRing : MonoBehaviour
 {
+    // new speed of the claw
+    public float m_fIncreasedSpeed = 40.0f;
+
+    // original speed of the claw
+    private float m_fOriginalSpeed = 0.0f;
+
+    private void Start()
+    {
+        // gets the original speed
+        m_fOriginalSpeed = GameObject.FindGameObjectWithTag("Claw").GetComponent<Claw>().m_fMoveSpeed;
+    }
+
+    // sets the speed back to normal
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Claw"))
         {
-            other.GetComponent<Claw>().m_fMoveSpeed = 25.0f;
+            other.GetComponent<Claw>().m_fMoveSpeed = m_fOriginalSpeed;
         }
     }
-
+    // increases the speed
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Claw"))
         {
-            other.GetComponent<Claw>().m_fMoveSpeed = 40.0f;
+            other.GetComponent<Claw>().m_fMoveSpeed = m_fIncreasedSpeed;
         }
     }
 }
