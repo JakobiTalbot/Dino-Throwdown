8fb6f9b6d5723fa27e1ddf29e5c9dac47cfec352
T-Scod <36467730+T-Scod@users.noreply.github.com>
2018-11-02 09:39:15 +1100

    - Added delay after respawning in wrecking bowl - Teleport players to audience stands when they are out

--- DinoThrowdown/Assets/Scripts/GameStateManagerWB.cs
+++ DinoThrowdown/Assets/Scripts/GameStateManagerWB.cs
@@ -11,8 +11,12 @@ public class GameStateManagerWB : MonoBehaviour
     public float m_fStartDelay = 3.0f;
     // delay before round ends
     public float m_fEndDelay = 3.0f;
+    // delay before players respawn
+    public float m_fRespawnDelay = 1.0f;
     // game over canvas
     public RestartGame m_gameOverCanvas;
+    // collection of seats in the audience stands
+    public GameObject[] m_seats = new GameObject[3];
 
     private GameObject m_canvas;
     private Vector3[] m_playerOriginalPositions;
@@ -28,9 +32,13 @@ public class GameStateManagerWB : MonoBehaviour
     private int[] m_cPlayerLives;
     // amount of remaining players
     private int m_cRemaining;
+    // collection of pick up managers used for delaying after respawn
+    private PlayerController.PickupTimer[] m_respawnDelays;
+    // used to check if a seat is occupied
+    private bool[] m_bSeats = new bool[3];
 
 	// Use this for initialization
-	void Awake()
+	private void Awake()
     {
         m_playerOriginalPositions = new Vector3[m_players.Length];
         m_playerOriginalRotations = new Quaternion[m_players.Length];
@@ -56,6 +64,12 @@ public class GameStateManagerWB : MonoBehaviour
 
         m_cPlayerLives = new int[m_players.Length];
         m_cRemaining = m_players.Length;
+        m_respawnDelays = new PlayerController.PickupTimer[m_players.Length];
+
+        for (int i = 0; i < m_bSeats.Length; i++)
+        {
+            m_bSeats[i] = false;
+        }
 
         if (CharacterManager.InstanceExists)
         {
@@ -69,9 +83,11 @@ public class GameStateManagerWB : MonoBehaviour
         for (int i = 0; i < m_players.Length; ++i)
         {
             // get original positions/rotations of each player
-            m_playerOriginalPositions[i] = new Vector3(m_players[i].transform.position.x, 20, m_players[i].transform.position.z);
+            m_playerOriginalPositions[i] = new Vector3(m_players[i].transform.position.x, 25, m_players[i].transform.position.z);
             m_playerOriginalRotations[i] = m_players[i].transform.rotation;
             m_cPlayerLives[i] = m_nLives;
+            m_respawnDelays[i].bFlag = false;
+            m_respawnDelays[i].fTimer = m_fRespawnDelay;
         }
 	}
 
@@ -140,19 +156,44 @@ public class GameStateManagerWB : MonoBehaviour
                 {
                     --m_cPlayerLives[i];
                     m_canvas.GetComponent<UI>().DisableRoundImage(m_players[i]);
+                    m_players[i].GetComponent<PlayerController>().m_bIsOut = false;
                     
                     if (m_cPlayerLives[i] == 0)
                     {
                         --m_cRemaining;
+                        m_players[i].GetComponent<PlayerController>().m_bIsOut = true;
+                        for (int j = 0; j < m_seats.Length; j++)
+                        {
+                            if (!m_bSeats[j])
+                            {
+                                m_players[i].transform.position = m_seats[j].transform.position;
+                                m_players[i].transform.rotation = m_seats[j].transform.rotation;
+                                m_bSeats[j] = true;
+                                break;
+                            }
+                        }
                     }
                     else
                     {
-                        m_players[i].GetComponent<PlayerController>().m_bIsOut = false;
                         m_players[i].transform.position = m_playerOriginalPositions[i];
                         m_players[i].transform.rotation = m_playerOriginalRotations[i];
-                        // reset player velocities
-                        m_players[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
-                        m_players[i].GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                    }
+
+                    m_players[i].GetComponent<Rigidbody>().isKinematic = true;
+                    m_respawnDelays[i].bFlag = true;
+                    // reset player velocities
+                    m_players[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
+                    m_players[i].GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                }
+
+                if (m_respawnDelays[i].bFlag)
+                {
+                    m_respawnDelays[i].fTimer -= Time.deltaTime;
+                    if (m_respawnDelays[i].fTimer <= 0.0f)
+                    {
+                        m_respawnDelays[i].fTimer = m_fRespawnDelay;
+                        m_respawnDelays[i].bFlag = false;
+                        m_players[i].GetComponent<Rigidbody>().isKinematic = false;
                     }
                 }
             }
