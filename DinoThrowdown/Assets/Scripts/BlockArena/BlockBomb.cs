a405921c35bca781cdf8583a25953a04498d92e2
JakobiTalbot <36465136+JakobiTalbot@users.noreply.github.com>
2018-11-01 16:50:38 +1100

    - Added new block arena scene with assets

---
+++ DinoThrowdown/Assets/Scripts/BlockArena/BlockBomb.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBomb : MonoBehaviour
+{
+    // amount of force from the bomb
+    public float m_fExplosionForce = 1000.0f;
+    // reference to the players layer
+    public LayerMask m_playerMask;
+    // effect radius of the explosion
+    public float m_fExplosionRadius = 5.0f;
+    // delay before the bomb explodes
+    public float m_fExplosionDelay = 1.5f;
+    // reference to the explosion particle
+    public ParticleSystem m_explosionParticle;
+    // size of the particle
+    public float m_fParticleSize = 1.3f;
+    // reference to the explosion audio
+    public AudioSource m_explosionAudio;
+    // strength for the controller to vibrate when hit by explosion
+    public float m_fVibrationStrength = 1.0f;
+    // time for the controller to vibrate when hit by explosion
+    public float m_fVibrationTime = 0.5f;
+    // maximum amount of blocks that can be destroyed by one explosion
+    public int m_nMaxBlocksToDestroy = 3;
+
+    // reference to the claw that drops the bomb
+    private Claw m_claw;
+    // determines if vibration is on
+    private bool m_bVibrationToggle = true;
+    // stores current number of ground blocks destroyed
+    private int m_nBlocksDestroyed = 0;
+
+    private void Start()
+    {
+        m_claw = GetComponentInParent<Claw>();
+        // gets whether vibration is on
+        if (OptionsManager.InstanceExists)
+        {
+            m_bVibrationToggle = OptionsManager.Instance.m_bVibration;
+        }
+    }
+
+    // find all the players in the area around the bomb and damage them
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Deadzone") || other.CompareTag("Claw") || other.CompareTag("Untagged"))
+        {
+            return;
+        }
+
+        // detatches the bomb from the claw
+        m_claw.m_bHasItem = false;
+        m_claw.m_bItemDrop = false;
+        gameObject.transform.parent = null;
+        // destroys the bomb after the delay
+        Destroy(gameObject, m_fExplosionDelay);
+    }
+
+    // calculates the amount of damage a target should take based on its position
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        // vector from the target to the shell
+        Vector3 explosionToTarget = targetPosition - transform.position;
+        // magnitude of the vector
+        float fExplosionDistance = explosionToTarget.magnitude;
+
+        // the distance relative to the explosion (starts off at 1 at the centre and ends at 0 at the radius)
+        float relativeDistance = (m_fExplosionRadius - fExplosionDistance) / m_fExplosionRadius;
+        // multiplies the distance by the maximum damage
+        float fDamage = relativeDistance/* * m_maxDamage*/;
+
+        // ensures that the damage is not negative
+        fDamage = Mathf.Max(0.0f, fDamage);
+
+        return fDamage;
+    }
+
+    private void OnDestroy()
+    {
+        // adds all colliders that collide with a sphere of radius m_fExplosionRadius to an array
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_fExplosionRadius);
+        // adds all players that collide with a sphere of radius m_fExplosionRadius to an array
+        Collider[] playerColliders = Physics.OverlapSphere(transform.position, m_fExplosionRadius, m_playerMask);
+
+        // iterates through all colliders
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            // tries to get the rigidbody from the target
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+            // checks if a rigidbody was not found
+            if (!targetRigidbody)
+            {
+                continue;
+            }
+
+            // on collision with ground block
+            if (colliders[i].CompareTag("Ground") && m_nBlocksDestroyed < m_nMaxBlocksToDestroy)
+            {
+                targetRigidbody.isKinematic = false;
+                m_nBlocksDestroyed++;
+            }
+            else if (colliders[i].CompareTag("Player"))
+            {
+                // adds an explosion force to the target from the bomb
+                targetRigidbody.AddExplosionForce(m_fExplosionForce + m_fExplosionForce * (colliders[i].GetComponent<Knockback>().m_fKnockbackMeter / 100.0f),
+                                                  transform.position, m_fExplosionRadius);
+                // calculates the damage to be dealt to the target
+                float fDamage = CalculateDamage(targetRigidbody.position);
+                colliders[i].GetComponent<Knockback>().m_fKnockbackMeter += fDamage * 20.0f;
+            }
+        }
+
+        // creates an explosion of the specified size
+        m_explosionParticle.gameObject.transform.parent = null;
+        m_explosionParticle.transform.localScale *= m_fParticleSize;
+        m_explosionParticle.Play();
+
+        // gets the sfx volume from the options
+        if (OptionsManager.InstanceExists)
+        {
+            m_explosionAudio.volume = OptionsManager.Instance.m_fSFXVolume * OptionsManager.Instance.m_fMasterVolume;
+        }
+        // plays the explosion audio
+        m_explosionAudio.Play();
+
+        // vibrates the controllers of all the hit players
+        if (m_bVibrationToggle)
+        {
+            for (int i = 0; i < playerColliders.Length; i++)
+            {
+                playerColliders[i].GetComponent<PlayerController>().SetVibration(m_fVibrationTime, m_fVibrationStrength);
+            }
+        }
+    }
+}
---
+++ DinoThrowdown/Assets/Scripts/BlockArena/GameStateManagerBlockArena.cs
@@ -0,0 +1,301 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManagerBlockArena : MonoBehaviour
+{
+    public int m_nRoundsToWin = 3;
+    public GameObject[] m_players;
+    public GameObject m_confetti;
+    // delay before round starts
+    public float m_fStartDelay = 3.0f;
+    // delay before round ends
+    public float m_fEndDelay = 3.0f;
+    // game over canvas
+    public RestartGame m_gameOverCanvas;
+
+    private GameObject[] m_cranes;
+    private GameObject[] m_claws;
+    private GameObject[] m_blocks;
+    private GameObject m_canvas;
+    private List<GameObject> m_playersRemaining;
+    private Vector3[] m_playerOriginalPositions;
+    private Quaternion[] m_playerOriginalRotations;
+    private Vector3[] m_clawOriginalPositions;
+    private Quaternion[] m_clawOriginalRotations;
+    private Vector3[] m_blockOriginalPositions;
+    private UI m_UI;
+    private int[] m_nRoundsWon;
+    private int m_nRoundNumber = 0;
+    private bool m_bPlayerWon = false;
+    // used to wait before the round starts
+    private WaitForSeconds m_startWait;
+    // used to wait before the round ends
+    private WaitForSeconds m_endWait;
+    // reference to background music
+    private AudioSource m_backgroundMusic;
+
+	// Use this for initialization
+	void Awake()
+    {
+        m_cranes = GameObject.FindGameObjectsWithTag("Crane");
+        m_claws = GameObject.FindGameObjectsWithTag("Claw");
+        m_blocks = GameObject.FindGameObjectsWithTag("Ground");
+        m_playerOriginalPositions = new Vector3[m_players.Length];
+        m_playerOriginalRotations = new Quaternion[m_players.Length];
+        m_clawOriginalPositions = new Vector3[m_claws.Length];
+        m_clawOriginalRotations = new Quaternion[m_claws.Length];
+        m_playersRemaining = new List<GameObject>();
+        m_nRoundsWon = new int[m_players.Length];
+        m_canvas = GameObject.FindGameObjectWithTag("Canvas");
+
+        m_backgroundMusic = GetComponent<AudioSource>();
+        if (OptionsManager.InstanceExists)
+        {
+            m_backgroundMusic.volume = OptionsManager.Instance.m_fMusicVolume * OptionsManager.Instance.m_fMasterVolume;
+            switch (OptionsManager.Instance.m_iRound)
+            {
+                case 0:
+                    m_nRoundsToWin = 1;
+                    break;
+                case 1:
+                    m_nRoundsToWin = 3;
+                    break;
+                case 2:
+                    m_nRoundsToWin = 5;
+                    break;
+            }
+        }
+
+        if (CharacterManager.InstanceExists)
+        {
+            for (int i = 0; i < m_players.Length; i++)
+            {
+                m_players[i].GetComponent<MeshFilter>().mesh = CharacterManager.Instance.m_dinoTypes[i];
+                m_players[i].GetComponent<MeshRenderer>().material = CharacterManager.Instance.m_colours[i];
+            }
+        }
+
+        for (int i = 0; i < m_players.Length; ++i)
+        {
+            // set each player's rounds won to 0
+            m_nRoundsWon[i] = 0;
+
+            // add each player to remaining players
+            m_playersRemaining.Add(m_players[i]);
+
+            // get original positions/rotations of each player
+            m_playerOriginalPositions[i] = m_players[i].transform.position;
+            m_playerOriginalRotations[i] = m_players[i].transform.rotation;
+        }
+
+        for (int i = 0; i < m_cranes.Length; ++i)
+        {
+            // get original claw position/rotation
+            m_clawOriginalPositions[i] = m_claws[i].transform.localPosition;
+            m_clawOriginalRotations[i] = m_claws[i].transform.localRotation;
+        }
+
+        for (int i = 0; i < m_blocks.Length; ++i)
+        {
+            m_blockOriginalPositions[i] = m_blocks[i].transform.position;
+        }
+	}
+
+    private void Start()
+    {
+        // sets the wait times for the start and end
+        m_startWait = new WaitForSeconds(m_fStartDelay);
+        m_endWait = new WaitForSeconds(m_fEndDelay);
+
+        // starts the game loop routine
+        StartCoroutine(GameLoop());
+    }
+
+    // loops through the game
+    private IEnumerator GameLoop()
+    {
+        // starts the start round coroutine
+        yield return StartCoroutine(RoundStarting());
+        // starts the playing round coroutine
+        yield return StartCoroutine(RoundPlaying());
+        // starts the end round coroutine
+        yield return StartCoroutine(RoundEnding());
+
+        // checks if there is a game winner
+        if (m_bPlayerWon)
+        {
+            // sets the game up for the end
+            DisableControls();
+            foreach (var player in m_players)
+            {
+                player.GetComponent<PlayerController>().m_bIsOut = true;
+            }
+            m_gameOverCanvas.gameObject.SetActive(true);
+        }
+        else
+        {
+            // starts the game loop coroutine
+            StartCoroutine(GameLoop());
+        }
+    }
+
+    // sets up the round
+    private IEnumerator RoundStarting()
+    {
+        // resets the round
+        NewRound();
+        // increment round number
+        ++m_nRoundNumber;
+        m_canvas.GetComponent<UI>().RoundText(m_nRoundNumber);
+        DisableControls();
+
+        // waits for the start delay
+        yield return m_startWait;
+    }
+
+    // runs the round
+    private IEnumerator RoundPlaying()
+    {
+        // enables the controls
+        EnableControls();
+        m_canvas.GetComponent<UI>().DisableText();
+
+        // loops while there is not one player left
+        while (m_playersRemaining.Count != 1)
+        {
+            for (int i = 0; i < m_playersRemaining.Count; ++i)
+            {
+                // remove player from players remaining if they are out
+                if (m_playersRemaining[i].GetComponent<PlayerController>().m_bIsOut
+                    || m_playersRemaining[i].GetComponent<PlayerController>().m_bInCrane)
+                {
+                    m_playersRemaining.RemoveAt(i);
+                }
+            }
+
+            for (int i = 0; i < m_players.Length; ++i)
+            {
+                // add players back
+                if (!m_players[i].GetComponent<PlayerController>().m_bIsOut
+                    && !m_players[i].GetComponent<PlayerController>().m_bInCrane
+                    && !m_playersRemaining.Contains(m_players[i]))
+                {
+                    m_playersRemaining.Add(m_players[i]);
+                }
+            }
+
+            // waits a frame
+            yield return null;
+        }
+    }
+
+    // ends the round
+    private IEnumerator RoundEnding()
+    {
+        for (int i = 0; i < m_players.Length; ++i)
+        {
+            // find which player it was
+            if (m_playersRemaining[0] == m_players[i])
+            {
+                // increment rounds won
+                ++m_nRoundsWon[i];
+                m_canvas.GetComponent<UI>().EnableRoundImage(m_players[i]);
+
+                // check if they won the amount of rounds needed to win game
+                if (m_nRoundsWon[i] == m_nRoundsToWin)
+                {
+                    m_confetti.SetActive(true);
+                    m_bPlayerWon = true;
+                    m_canvas.GetComponent<UI>().EnablePlayerWon(m_players[i]);
+                    m_canvas.GetComponent<UI>().DisableText();
+                }
+            }
+        }
+
+        // waits for the end delay
+        yield return m_endWait;
+    }
+
+    private void NewRound()
+    {
+        m_playersRemaining.Clear();
+        for (int i = 0; i < m_players.Length; ++i)
+        {
+            // add players to remaining players
+            m_playersRemaining.Add(m_players[i]);
+
+            // reset player position
+            m_players[i].transform.position = m_playerOriginalPositions[i];
+            // reset player rotation
+            m_players[i].transform.rotation = m_playerOriginalRotations[i];
+            // reset player velocities
+            m_players[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
+            m_players[i].GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            // reset player knockback meter
+            m_players[i].GetComponent<Knockback>().m_fKnockbackMeter = 0.0f;
+            // set kinematic to false
+            m_players[i].GetComponent<Rigidbody>().isKinematic = false;
+            // reset powerups
+            m_players[i].GetComponent<PlayerController>().m_cruiseControl.bFlag = false;
+            m_players[i].GetComponent<PlayerController>().m_cruiseControl.fTimer = 0.0f;
+            m_players[i].GetComponent<Knockback>().m_shield.bFlag = false;
+            m_players[i].GetComponent<Knockback>().m_shield.fTimer = 0.0f;
+            m_players[i].GetComponent<PlayerController>().m_weaponSize.bFlag = false;
+            m_players[i].GetComponent<PlayerController>().m_weaponSize.fTimer = 0.0f;
+            m_players[i].GetComponent<PlayerController>().m_weapon.transform.localScale = m_players[i].GetComponent<PlayerController>().m_v3BaseWeaponScale;
+            m_players[i].GetComponent<PlayerController>().m_weapon.transform.localPosition = m_players[i].GetComponent<PlayerController>().m_v3BaseWeaponPosition;
+            // reset player status
+            m_players[i].GetComponent<PlayerController>().m_bInCrane = false;
+            m_players[i].GetComponent<PlayerController>().m_bIsOut = false;
+            m_players[i].GetComponent<PlayerController>().m_bWeaponHit = false;
+            m_players[i].GetComponent<PlayerController>().StopAttack();
+            m_players[i].GetComponent<PlayerController>().m_cAttackAmount = 0;
+            // set claw to null
+            m_players[i].GetComponent<PlayerController>().m_claw = null;
+            // reset dash timer
+            m_players[i].GetComponent<Dash>().ResetTimer();
+        }
+
+        for (int i = 0; i < m_cranes.Length; ++i)
+        {
+            m_cranes[i].GetComponent<CraneManager>().m_bOccupied = false;
+            m_cranes[i].GetComponent<CraneManager>().m_player = null;
+
+            m_claws[i].transform.localPosition = m_clawOriginalPositions[i];
+            m_claws[i].transform.localRotation = m_clawOriginalRotations[i];
+            m_claws[i].GetComponent<Claw>().m_bDropped = false;
+            m_claws[i].GetComponent<Claw>().m_bHasItem = false;
+            m_claws[i].GetComponent<Claw>().m_bItemDrop = false;
+            m_claws[i].GetComponent<Claw>().m_item = null;
+            m_claws[i].GetComponent<Claw>().m_bHasPlayer = false;
+            m_claws[i].GetComponent<Claw>().m_pickedUpPlayer = null;
+            Destroy(m_claws[i].GetComponent<Claw>().m_item);
+            m_claws[i].GetComponentInChildren<LineRenderer>().SetPosition(1, Vector3.zero);
+        }
+
+        for (int i = 0; i < m_blocks.Length; ++i)
+        {
+            m_blocks[i].transform.position = m_blockOriginalPositions[i];
+            m_blocks[i].transform.rotation = Quaternion.Euler(Vector3.zero);
+            m_blocks[i].GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+
+    // sets all players to kinematic
+    private void DisableControls()
+    {
+        foreach (var player in m_players)
+        {
+            player.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+    // sets all players to not kinematic
+    private void EnableControls()
+    {
+        foreach (var player in m_players)
+        {
+            player.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
