1779113de095473e3235c9009c4343401f79d215
JakobiTalbot <36465136+JakobiTalbot@users.noreply.github.com>
2018-11-01 14:46:14 +1100

    - Added OutOfBoundsWreckingBowl script to work with WreckingBowl game mode

---
+++ DinoThrowdown/Assets/Scripts/OutOfBoundsWreckingBowl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBoundsWreckingBowl : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // checks if the collider is a player
+        if (other.CompareTag("Player"))
+        {
+            if (other.GetComponent<Rigidbody>().isKinematic)
+            {
+                return;
+            }
+
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            if (OptionsManager.InstanceExists)
+            {
+                other.GetComponent<AudioSource>().volume = OptionsManager.Instance.m_fSFXVolume * OptionsManager.Instance.m_fMasterVolume;
+            }
+
+            // plays the audio
+            other.GetComponent<AudioSource>().Play();
+
+            // sets the player as out if all cranes are occupied
+            playerController.m_bIsOut = true;
+        }
+    }
+}
