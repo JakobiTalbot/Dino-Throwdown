0cee3db6699ba5663a6b36d4dfcdda9383259a33
T-Scod <36467730+T-Scod@users.noreply.github.com>
2018-11-02 17:01:01 +1100

    - Made a new wrecking ball using hinge joints

--- DinoThrowdown/Assets/Scripts/WreckingBall.cs
+++ DinoThrowdown/Assets/Scripts/WreckingBall.cs
@@ -6,16 +6,14 @@ public class WreckingBall : MonoBehaviour
 {
     // time to wait before swinging again after finishing last swing
     public float m_fSwingTimeToWait = 10.0f;
-    // speed to swing
-    public float m_fSwingSpeed = 100.0f;
+    //// speed to swing
+    //public float m_fSwingSpeed = 100.0f;
     public LineRenderer m_warningLine;
 
-    [HideInInspector]
-    public float m_fSwingTimer;
-
+    private float m_fSwingTimer;
     private Quaternion m_swingEnd;
     private Vector3 m_v3SwingDir;
-    private bool m_bSwinging = false;
+    private bool m_bSwinging = true;
 
 	// Use this for initialization
 	void Awake()
@@ -27,40 +25,49 @@ public class WreckingBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // decrement swing timer
-        m_fSwingTimer -= Time.deltaTime;
-
-        if (m_fSwingTimer <= 0.0f && !m_bSwinging)
-        {
-            // get random y rotation
-            float fRandY = Random.Range(0.0f, 360.0f);
-
-            // set end rotation
-            m_swingEnd = Quaternion.Euler(0.0f, fRandY, 90.0f);
-            // set start rotation
-            transform.rotation = Quaternion.Euler(0.0f, fRandY, -90.0f);
-
-            // find swing direction
-            m_v3SwingDir = (transform.rotation.eulerAngles - m_swingEnd.eulerAngles).normalized;
-            m_bSwinging = true;
-
-            m_warningLine.GetComponent<LineRenderer>().enabled = true;
-            // set line rotation
-            m_warningLine.transform.rotation = Quaternion.Euler(0.0f, fRandY, 0.0f);
-        }
-
         if (m_bSwinging)
         {
-            // rotate wrecking ball (swing)
-            transform.Rotate(m_v3SwingDir * m_fSwingSpeed * Time.deltaTime);
-
-            // end swing if close to end rotation
-            if ((m_swingEnd.eulerAngles - transform.rotation.eulerAngles).sqrMagnitude < 50.0f)
+            m_fSwingTimer -= Time.deltaTime;
+            if (m_fSwingTimer <= 0)
             {
-                m_bSwinging = false;
-                m_fSwingTimer = m_fSwingTimeToWait;
-                m_warningLine.GetComponent<LineRenderer>().enabled = false;
+                float fRandYRotation = Random.Range(0.0f, 360.0f);
             }
         }
+
+        //// decrement swing timer
+        //m_fSwingTimer -= Time.deltaTime;
+
+        //if (m_fSwingTimer <= 0.0f && !m_bSwinging)
+        //{
+        //    // get random y rotation
+        //    float fRandY = Random.Range(0.0f, 360.0f);
+
+        //    // set end rotation
+        //    m_swingEnd = Quaternion.Euler(0.0f, fRandY, 90.0f);
+        //    // set start rotation
+        //    transform.rotation = Quaternion.Euler(0.0f, fRandY, -90.0f);
+
+        //    // find swing direction
+        //    m_v3SwingDir = (transform.rotation.eulerAngles - m_swingEnd.eulerAngles).normalized;
+        //    m_bSwinging = true;
+
+        //    m_warningLine.GetComponent<LineRenderer>().enabled = true;
+        //    // set line rotation
+        //    m_warningLine.transform.rotation = Quaternion.Euler(0.0f, fRandY, 0.0f);
+        //}
+
+        //if (m_bSwinging)
+        //{
+        //    // rotate wrecking ball (swing)
+        //    transform.Rotate(m_v3SwingDir * m_fSwingSpeed * Time.deltaTime);
+
+        //    // end swing if close to end rotation
+        //    if ((m_swingEnd.eulerAngles - transform.rotation.eulerAngles).sqrMagnitude < 50.0f)
+        //    {
+        //        m_bSwinging = false;
+        //        m_fSwingTimer = m_fSwingTimeToWait;
+        //        m_warningLine.GetComponent<LineRenderer>().enabled = false;
+        //    }
+        //}
 	}
 }
