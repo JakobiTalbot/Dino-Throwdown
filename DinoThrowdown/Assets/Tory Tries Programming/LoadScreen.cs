3273e22fb72f054e16efb2e17cb109715e40137f
Tory-Kruger <39174598+Tory-Kruger@users.noreply.github.com>
2018-11-08 16:12:58 +1100

    -Added new Maintrasitiontests.unity scene -Teacher made scene fade script

---
+++ DinoThrowdown/Assets/Tory Tries Programming/LoadScreen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadScreen : MonoBehaviour {
+
+    public Image loadScreen;
+
+	// Use this for initialization
+	void Start () {
+        loadScreen.gameObject.SetActive(true);
+        loadScreen.canvasRenderer.SetAlpha(1f);
+        loadScreen.CrossFadeAlpha(0f, 2f, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            FadeOut();
+        }
+
+        
+
+	}
+
+    void FadeOut()
+    {
+        Invoke("LoadScene", 2f);
+        loadScreen.CrossFadeAlpha(1f, 2f, true);
+    }
+
+    void LoadScene()
+    {
+        Debug.Log("Loading");
+    }
+}
