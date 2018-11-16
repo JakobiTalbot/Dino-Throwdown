243fd8b9e0b536523f6d81ddc5705a4e07347273
JakobiTalbot <36465136+JakobiTalbot@users.noreply.github.com>
2018-11-16 14:25:54 +1100

    - Swapped indexing of character icons

--- DinoThrowdown/Assets/Scripts/UI.cs
+++ DinoThrowdown/Assets/Scripts/UI.cs
@@ -38,7 +38,7 @@ public class UI : MonoBehaviour
                 CharacterManager charManager = CharacterManager.Instance;
                 for (int j = 0; j < m_playerPortraits.Length; ++j)
                 {
-                    m_playerPortraits[j].GetComponent<RawImage>().texture = m_portraits.rows[charManager.m_nDinoColourIndex[j]].row[charManager.m_nDinoTypeIndex[j]];
+                    m_playerPortraits[j].GetComponent<RawImage>().texture = m_portraits.rows[charManager.m_nDinoTypeIndex[j]].row[charManager.m_nDinoColourIndex[j]];
                 }
             }
             
