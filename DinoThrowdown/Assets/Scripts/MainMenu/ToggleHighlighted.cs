8aafa928a1ec6560c50404cf8893cc126fd0ec08
JakobiTalbot <36465136+JakobiTalbot@users.noreply.github.com>
2018-11-23 16:28:10 +1100

    - Added new scene with main menu toggle highlights

---
+++ DinoThrowdown/Assets/Scripts/MainMenu/ToggleHighlighted.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ToggleHighlighted : MonoBehaviour
+{
+    public GameObject m_highlight;
+
+    public void OnSelect()
+    {
+        m_highlight.SetActive(true);
+    }
+    public void OnDeselect()
+    {
+        m_highlight.SetActive(false);
+    }
+}
