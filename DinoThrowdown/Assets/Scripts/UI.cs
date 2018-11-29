829ec076f0eee706665fb2f003a348db0d9fe00d
JakobiTalbot <36465136+JakobiTalbot@users.noreply.github.com>
2018-11-29 11:28:35 +1100

    - Changed the way the round images are displayed

--- DinoThrowdown/Assets/Scripts/UI.cs
+++ DinoThrowdown/Assets/Scripts/UI.cs
@@ -76,30 +76,30 @@ public class UI : MonoBehaviour
             }
         }
 
-        int nRounds = 0;
-        // set round images to active based on how many rounds there are
-        if (OptionsManager.InstanceExists)
-        {
-            switch (OptionsManager.Instance.m_iRound)
-            {
-                case 0:
-                    nRounds = 1;
-                    break;
-                case 1:
-                    nRounds = 3;
-                    break;
-                case 2:
-                    nRounds = 5;
-                    break;
-            }
-        }
-        for (int i = 0; i < nRounds; ++i)
-        {
-            m_p1RoundImages[i].SetActive(true);
-            m_p2RoundImages[i].SetActive(true);
-            m_p3RoundImages[i].SetActive(true);
-            m_p4RoundImages[i].SetActive(true);
-        }
+        //int nRounds = 0;
+        //// set round images to active based on how many rounds there are
+        //if (OptionsManager.InstanceExists)
+        //{
+        //    switch (OptionsManager.Instance.m_iRound)
+        //    {
+        //        case 0:
+        //            nRounds = 1;
+        //            break;
+        //        case 1:
+        //            nRounds = 3;
+        //            break;
+        //        case 2:
+        //            nRounds = 5;
+        //            break;
+        //    }
+        //}
+        //for (int i = 0; i < nRounds; ++i)
+        //{
+        //    m_p1RoundImages[i].SetActive(true);
+        //    m_p2RoundImages[i].SetActive(true);
+        //    m_p3RoundImages[i].SetActive(true);
+        //    m_p4RoundImages[i].SetActive(true);
+        //}
     }
 
 	// Update is called once per frame
@@ -206,9 +206,9 @@ public class UI : MonoBehaviour
 
                 for (int j = 0; j < GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameStateManager>().m_nRoundsToWin; ++j)
                 {
-                    if (playerRoundImages[j].GetComponent<RawImage>().texture != m_roundWinIcon)
+                    if (!playerRoundImages[j].activeSelf)
                     {
-                        playerRoundImages[j].GetComponent<RawImage>().texture = m_roundWinIcon;
+                        playerRoundImages[j].SetActive(true);
                         return;
                     }
                 }
